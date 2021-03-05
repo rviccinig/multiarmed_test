f30f18ca336d73501ab3bd8cd8a3dbba0bccfe4d
Reinaldo <rviccinig@gmail.com>
2021-03-05 12:11:41 -0700

    v 10 added function  to randomly display sign in pages

--- multiarmed_test/core/views.py
+++ multiarmed_test/core/views.py
@@ -4,12 +4,16 @@ from flask import render_template, url_for, flash, redirect, request, Blueprint
 from multiarmed_test import db
 from multiarmed_test.models import User
 from multiarmed_test.core.forms import RegistrationForm
+import numpy as np
 
 users_blueprint = Blueprint('users', __name__)
 
+# function that displays a random page depending on the probability
+templates=['Shopify_sign_in_1.html','Shopify_sign_in_2.html','Shopify_sign_in_3.html']
+prob=[0.34,0.34,0.32]
+
+
 # register a users
-
-
 @users_blueprint.route('/', methods=['GET', "POST"])
 def register():
     form = RegistrationForm()
@@ -21,4 +25,6 @@ def register():
         db.session.commit()
         flash('thanks for registration!')
         return redirect(url_for('thankyou.html'))
-    return render_template('signup1.html',form=form)
+    choice=np.random.choice(templates,1,p=prob)[0]
+#choice is an array that produces the path for one of the html but I scope the 0 element.
+    return render_template(choice,form=form)
